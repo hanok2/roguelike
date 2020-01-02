4c6c3e3345b4a3e1b75b1a28485a7702475d98ad
Erik Lunna <eslunna@gmail.com>
2020-01-01 19:14:19 -0600

    Added comments.

--- engine.py
+++ engine.py
@@ -9,16 +9,25 @@ def main():
     img_file = 'images/arial10x10.png'
 
     tcod.console_set_custom_font(img_file, tcod.FONT_TYPE_GREYSCALE | tcod.FONT_LAYOUT_TCOD)
+
+    # Creates the screen.
+    # Boolean specifies full screen
     tcod.console_init_root(screen_width, screen_height, 'libtcod tutorial revised', False)
 
     # Game loop
     while not tcod.console_is_window_closed():
         tcod.console_set_default_foreground(0, tcod.white)
+
+        # Game symbol
         tcod.console_put_char(0, 1, 1, '@', tcod.BKGND_NONE)
+
+        # Presents everything on screen
         tcod.console_flush()
 
+        # Get keyboard input
         key = tcod.console_check_for_keypress()
 
+        # Check input
         if key.vk == tcod.KEY_ESCAPE:
             return True
 
