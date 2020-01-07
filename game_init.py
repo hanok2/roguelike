5ea3f8e2512d5c095e94a35a3c392a1bb5940d93
Erik Lunna <eslunna@gmail.com>
2020-01-07 03:15:07 -0600

    Fixed new game function - game always re-initializes now.

--- game_init.py
+++ game_init.py
@@ -9,63 +9,62 @@ from messages import MsgLog
 from render_functions import RenderOrder
 from states import States
 
-
-# Player components
-fighter_comp = Fighter(hp=100, defense=1, power=2)
-inv_comp = Inventory(26)
-lvl_comp = Level()
-equipment_comp = Equipment()
-
-# Create entities
-hero = Entity(
-    0, 0,
-    '@',
-    tcod.white,
-    'Player',
-    blocks=True,
-    render_order=RenderOrder.ACTOR,
-    fighter=fighter_comp,
-    inv=inv_comp,
-    lvl=lvl_comp,
-    equipment=equipment_comp,
-)
-
-entities = [hero]
-
-equippable_comp = Equippable(EquipmentSlots.MAIN_HAND, power_bonus=2)
-dagger = Entity(
-    0, 0,
-    '(',
-    tcod.sky,
-    'Dagger',
-    equippable=equippable_comp
-)
-hero.inv.add_item(dagger)
-hero.equipment.toggle_equip(dagger)
-
-# Initialize the game map
-game_map = GameMap(
-    config.map_width,
-    config.map_height
-)
-
-game_map.make_map(
-    config.max_rooms,
-    config.room_min_len,
-    config.room_max_len,
-    config.map_width,
-    config.map_height,
-    hero,
-    entities,
-)
-
-msg_log = MsgLog(
-    config.msg_x,
-    config.msg_width,
-    config.msg_height
-)
-
-state = States.HERO_TURN
-
 def get_game_data():
+    # Player components
+    fighter_comp = Fighter(hp=100, defense=1, power=2)
+    inv_comp = Inventory(26)
+    lvl_comp = Level()
+    equipment_comp = Equipment()
+
+    # Create entities
+    hero = Entity(
+        0, 0,
+        '@',
+        tcod.white,
+        'Player',
+        blocks=True,
+        render_order=RenderOrder.ACTOR,
+        fighter=fighter_comp,
+        inv=inv_comp,
+        lvl=lvl_comp,
+        equipment=equipment_comp,
+    )
+
+    entities = [hero]
+
+    equippable_comp = Equippable(EquipmentSlots.MAIN_HAND, power_bonus=2)
+    dagger = Entity(
+        0, 0,
+        '(',
+        tcod.sky,
+        'Dagger',
+        equippable=equippable_comp
+    )
+    hero.inv.add_item(dagger)
+    hero.equipment.toggle_equip(dagger)
+
+    # Initialize the game map
+    game_map = GameMap(
+        config.map_width,
+        config.map_height
+    )
+
+    game_map.make_map(
+        config.max_rooms,
+        config.room_min_len,
+        config.room_max_len,
+        config.map_width,
+        config.map_height,
+        hero,
+        entities,
+    )
+
+    msg_log = MsgLog(
+        config.msg_x,
+        config.msg_width,
+        config.msg_height
+    )
+
+    state = States.HERO_TURN
+
     return hero, entities, game_map, msg_log, state
