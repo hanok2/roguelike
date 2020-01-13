4ad7158c29a84dbc6edcf34d2f0055ec8fd383f9
Erik Lunna <eslunna@gmail.com>
2020-01-12 21:27:08 -0600

    Added tests for game_init.

--- src/game_init.py
+++ src/game_init.py
@@ -6,6 +6,7 @@ from .states import States
 
 
 # todo: Convert to a Game object, tracking all game data....
+
 def get_game_data():
     # Create the hero
     hero = player.get_hero()
@@ -13,13 +14,18 @@ def get_game_data():
     # Initialize the Dungeon
     dungeon = maps.Dungeon(hero)
 
+    # Initialize the MsgLog
     msg_log = MsgLog(
         x=1,
         width=config.scr_width,
         height=config.msg_height
     )
 
+    # Set the initial game state
     state = States.HERO_TURN
+
+    # Set the turn counter to 0
     turn = 0
 
+    # Return a tuple to start the game
     return dungeon, msg_log, state, turn
