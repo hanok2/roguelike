3252c1e568c4d911e3353d20b609c2821889ce1a
Erik Lunna <eslunna@gmail.com>
2020-01-19 07:29:21 -0600

    Created a Game class to manage all game info.

--- src/game.py
+++ src/game.py
@@ -1,10 +1,35 @@
 from . import config
 from . import dungeon
+from . import fov
 from . import player
 from .messages import MsgLog
 
 
-# todo: Convert to a Game object, tracking all game data....
+class Game(object):
+    def __init__(self):
+        # Create the hero
+        new_hero = player.get_hero()
+
+        # Initialize the Dungeon
+        new_dungeon = dungeon.Dungeon(new_hero)
+
+        # Initialize the MsgLog
+        msg_log = MsgLog(x=1, width=config.scr_width, height=config.msg_height)
+
+        self.hero = new_hero
+        self.dungeon = new_dungeon
+        self.stage = self.dungeon.get_stage()
+        self.msg_log = msg_log
+        self.state = config.States.HERO_TURN
+        self.prev_state = self.state
+        self.turns = 0
+        self.targeting_item = None
+        self.next_action = None
+
+        self.fov_recompute = True
+        self.fov_map = fov.initialize_fov(self.stage)
+        self.redraw = False
+
 
 def get_game_data():
     # Create the hero
--- tests/test_game.py
+++ tests/test_game.py
@@ -1,7 +1,77 @@
+from ..src import config
 from ..src import game
 from ..src import dungeon
 from ..src import messages
+from ..src import player
+from ..src import stages
 from ..src.config import States
+
+
+""" Tests for Game class """
+
+
+def test_Game_init__hero():
+    g = game.Game()
+    assert isinstance(g.hero, player.Player)
+
+
+def test_Game_init__dungeon_is_None():
+    g = game.Game()
+    assert isinstance(g.dungeon, dungeon.Dungeon)
+    assert g.dungeon.current_stage == 0
+
+
+def test_Game_init__stage():
+    g = game.Game()
+    assert isinstance(g.stage, stages.Stage)
+
+
+def test_Game_init__msg_log_is_None():
+    g = game.Game()
+    assert isinstance(g.msg_log, messages.MsgLog)
+
+
+def test_Game_init__state_is_None():
+    g = game.Game()
+    assert g.state == config.States.HERO_TURN
+
+
+def test_Game_init__prev_state_equals_state():
+    g = game.Game()
+    assert g.prev_state == g.state
+
+
+def test_Game_init__turns_is_0():
+    g = game.Game()
+    assert g.turns == 0
+
+
+def test_Game_init__targeting_item_is_None():
+    g = game.Game()
+    assert g.targeting_item is None
+
+
+def test_Game_init__next_action_is_None():
+    g = game.Game()
+    assert g.next_action is None
+
+
+def test_Game_init__fov_recompute_is_True():
+    g = game.Game()
+    assert g.fov_recompute
+
+
+def test_Game_init__fov_map():
+    g = game.Game()
+    assert g.fov_map
+
+
+def test_Game_init__redraw_is_False():
+    g = game.Game()
+    assert g.redraw is False
+
+
+""" Tests for get_game_data."""
 
 
 def test_get_game_data__Dungeon():
