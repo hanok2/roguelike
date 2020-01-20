49a5d3529ef357445d37d4bf28a1b3e5adfba32b
Erik Lunna <eslunna@gmail.com>
2020-01-19 22:55:22 -0600

    Implemented a simple FIFO ActionQueue, tested basic functions.

--- src/actionqueue.py
+++ src/actionqueue.py
@@ -1,7 +1,21 @@
 class ActionQueue(object):
     """ A FIFI queue. """
+    # todo: Protect queue
+    # todo: Implement put
+    # todo: Implement get
+
     def __init__(self):
         self.queue = []
 
     def __len__(self):
         return len(self.queue)
+
+    def put(self, item):
+        self.queue.append(item)
+
+    def get(self):
+        item = self.queue.pop(0)
+        return item
+
+    def empty(self):
+        return len(self.queue) == 0
--- tests/test_actionqueue.py
+++ tests/test_actionqueue.py
@@ -12,3 +12,35 @@ def test_ActionQueue_init():
 def test_ActionQueue_len():
     q = actionqueue.ActionQueue()
     assert len(q) == 0
+
+
+def test_ActionQueue_put():
+    q = actionqueue.ActionQueue()
+    q.put(1)
+    q.put(2)
+    assert q.queue == [1, 2]
+
+
+def test_ActionQueue_get():
+    q = actionqueue.ActionQueue()
+    q.put(1)
+    q.put(2)
+    result = q.get()
+    assert result == 1
+
+
+def test_ActionQueue_empty__empty_returns_True():
+    q = actionqueue.ActionQueue()
+    assert q.empty()
+
+
+def test_ActionQueue_empty__populated_returns_False():
+    q = actionqueue.ActionQueue()
+    q.put(1)
+    assert not q.empty()
+
+
+# def test_ActionQueue_get__empty_queue():
+# def test_ActionQueue_iterator?():
+# def test_ActionQueue_cannot_manipulate_the_queue():
+
