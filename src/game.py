7dc3802ebcf86dfd20411e1c5bfab2254084d882
Erik Lunna <eslunna@gmail.com>
2020-01-20 10:16:03 -0600

    Removed game.get_game_data.

--- src/game.py
+++ src/game.py
@@ -33,27 +33,3 @@ class Game(object):
         self.fov_recompute = True
         self.fov_map = fov.initialize_fov(self.stage)
         self.redraw = False
-
-
-def get_game_data():
-    # Create the hero
-    hero = player.get_hero()
-
-    # Initialize the Dungeon
-    new_dungeon = dungeon.Dungeon(hero)
-
-    # Initialize the MsgLog
-    msg_log = MsgLog(
-        x=1,
-        width=config.scr_width,
-        height=config.msg_height
-    )
-
-    # Set the initial game state
-    state = config.States.HERO_TURN
-
-    # Set the turn counter to 0
-    turn = 0
-
-    # Return a tuple to start the game
-    return new_dungeon, msg_log, state, turn
--- tests/test_game.py
+++ tests/test_game.py
@@ -70,26 +70,3 @@ def test_Game_init__fov_map():
 def test_Game_init__redraw_is_False():
     g = game.Game()
     assert g.redraw is False
-
-
-""" Tests for get_game_data."""
-
-
-def test_get_game_data__Dungeon():
-    _dungeon, msg_log, state, turn = game.get_game_data()
-    assert isinstance(_dungeon, dungeon.Dungeon)
-
-
-def test_get_game_data__MsgLog():
-    _dungeon, msg_log, state, turn = game.get_game_data()
-    assert isinstance(msg_log, messages.MsgLog)
-
-
-def test_get_game_data__State_is_HERO_TURN():
-    _dungeon, msg_log, state, turn = game.get_game_data()
-    assert state == States.HERO_TURN
-
-
-def test_get_game_data__turn_is_0():
-    _dungeon, msg_log, state, turn = game.get_game_data()
-    assert turn == 0
